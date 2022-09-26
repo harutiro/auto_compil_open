1020684cd92e5f4be9393bd14e6909947e728fa6
harutiro <harutiro2027@outlook.jp>
2022-09-26 20:53:55 +0900

    fact:ファイルパスを取得した

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -14,13 +15,23 @@ import androidx.compose.ui.window.application
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+    var text by remember { mutableStateOf("") }
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Column {
+
+            Button(onClick = {
+                val files = Compile("/Users/k22120kk/Desktop/classCode").fileGet()
+                files.forEach {
+                    text += it + "\n"
+                }
+            }) {
+                Text("いい感じにファイルを取得する")
+            }
+
+            Text(
+                text
+            )
         }
     }
 }
