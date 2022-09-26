5e2381e57f942bb0601e0b646736b5b1a4707eb5
harutiro <harutiro2027@outlook.jp>
2022-09-26 21:30:27 +0900

    fact: 解凍の部分を行った。

--- src/jvmMain/kotlin/Compile.kt
+++ src/jvmMain/kotlin/Compile.kt
@@ -1,21 +1,80 @@
-import java.io.BufferedReader
-import java.io.File
-import java.io.IOException
-import java.io.InputStreamReader
+import java.io.*
 import java.nio.charset.Charset
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
+import kotlin.io.path.Path
 
 
 class Compile (
-    val path: String
+    private val path: String
 
     ) {
 
-    fun fileGet(): MutableList<String> {
+    // 最大ファイル数
+    private val unzipMaxEntries: Int = 1024 * 5
+    // 単体の最大ファイルサイズ
+    private val unzipMaxFileSize: Long = 1024L * 1024L * 1024L * 5L // 5GiB
+    // 全ファイルの合計最大ファイルサイズ
+    private val unzipMaxTotalSize: Long = 1024L * 1024L * 1024L * 5L // 5GiB
+    // バッファサイズ (参考値: https://gihyo.jp/admin/clip/01/fdt/200810/31)
+    private val unzipBufferSize: Int = 1024 * 1024 // 1MiB
 
-        val files = mutableListOf<String>()
+
+    fun unzipFile(targetFilePath: Path, destDirPath: Path = targetFilePath.parent, zipFileCoding: Charset = Charset.forName("Shift_JIS")) {
+        ZipInputStream(Files.newInputStream(targetFilePath), zipFileCoding).use { f ->
+            var zipEntry: ZipEntry?
+            var nEntries = 0
+            var totalReads = 0L
+            val buffer = ByteArray(unzipBufferSize)
+            while (f.nextEntry.also { zipEntry = it } != null) {
+                val entryPath = Paths.get(zipEntry!!.name).normalize()
+                if (entryPath.startsWith(Paths.get(".."))) {
+                    throw IllegalStateException("File is outside extraction target directory.")
+                }
+                if (nEntries++ >= unzipMaxEntries) {
+                    throw IllegalStateException("Too many files to unzip.")
+                }
+
+                val dst = destDirPath.resolve(entryPath)
+                if (zipEntry!!.isDirectory) {
+                    Files.createDirectories(dst)
+                } else {
+                    // System.err.println("inflating: $dst")
+                    Files.createDirectories(dst.parent)
+
+                    var totalFileReads = 0L
+                    var nReads: Int
+                    FileOutputStream(dst.toFile()).use { fos ->
+                        BufferedOutputStream(fos).use { out ->
+                            while (f.read(buffer, 0, buffer.size).also { nReads = it } != -1) {
+                                totalReads += nReads
+                                if (totalReads > unzipMaxTotalSize) {
+                                    throw IllegalStateException("Total file size being unzipped is too big.")
+                                }
+                                totalFileReads += nReads
+                                if (totalFileReads > unzipMaxFileSize) {
+                                    throw IllegalStateException("File being unzipped is too big.")
+                                }
+                                out.write(buffer, 0, nReads)
+                            }
+                            out.flush()
+                        }
+                    }
+                    f.closeEntry()
+                }
+            }
+        }
+    }
+
+    fun fileGet(): MutableList<Path> {
+
+        val files = mutableListOf<Path>()
 
         File(path).walk().forEach {
-            files.add(it.path)
+            files.add(it.toPath())
         }
 
 //        // 1. ProcessBuilderインスタンスを生成する
--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -11,22 +11,50 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import java.nio.file.Files
+import java.nio.file.Path
+import kotlin.io.path.pathString
 
 @Composable
 @Preview
 fun App() {
     var text by remember { mutableStateOf("") }
 
+    val compile = Compile("/Users/k22120kk/Desktop/classCode")
+
     MaterialTheme {
         Column {
 
             Button(onClick = {
-                val files = Compile("/Users/k22120kk/Desktop/classCode").fileGet()
+                val files = compile.fileGet()
                 files.forEach {
-                    text += it + "\n"
+                    text += it.pathString + "\n"
                 }
             }) {
                 Text("いい感じにファイルを取得する")
+            }
+
+            Button(onClick = {
+                val files = compile.fileGet()
+                files.forEach {
+                    if(!Files.isDirectory(it)){
+
+                        val fileName:String = it.fileName.pathString
+                        //拡張子の取得
+                        val fileNameWithoutExtension = fileName.split(".").lastOrNull()
+                        //ファイル名の取得（拡張子抜き）
+                        val fileNameWithInExtension = fileName.split(".")[0]
+
+                        if(fileNameWithoutExtension == "zip"){
+                            println(files[0].resolve(fileNameWithInExtension))
+                            compile.unzipFile(it , files[0].resolve(fileNameWithInExtension))
+                        }
+                    }
+//                    println(it.fileName)
+                }
+
+            }) {
+                Text("いい感じにファイルを解凍する")
             }
 
             Text(
